83aeb03afe319a127d019c6a026c3a56d2eb3bf7
Selma <anaselma1962@gmail.com>
2020-04-02 16:27:11 -0300

    adding names and gits

--- src/components/AboutContent/index.js
+++ src/components/AboutContent/index.js
@@ -4,214 +4,138 @@ import './index.css';
 
 export default function AboutContent() {
   return (
-    <section className="text-center">
-      <section className="page-header mt-3">
-        <h4>Quem Somos</h4>
-        <hr />
-      </section>
+  	<section className="text-center">
+	    <section className="page-header mt-3">
+	    	<h4>Quem Somos</h4>
+	    	<hr />
+	    </section>
 
-      <section className="container">
-        <p>
-          Somos uma equipe de startup com o objetivo de centralizar as melhores
-          vagas disponíveis em outras plataformas e apresentá-las de maneira
-          fácil e ágil para pessoas em busca de oportunidades.
-        </p>
+	    <section className="container">
+	    	<p>
+		    	Somos uma equipe de startup com o objetivo de centralizar as melhores 
+		    	vagas disponíveis em outras plataformas e apresentá-las de maneira fácil
+		    	e ágil para pessoas em busca de oportunidades.
+		    </p>
 
-        <p>
-          Também contamos com um chatbot de alarmes para anúncios de novas vagas
-        </p>
-      </section>
+		    <p>
+		    	Também contamos com um chatbot de alarmes para anúncios de novas vagas
+		    </p>
+	    </section>
 
-      <section className="github mt-5">
-        <section className="page-header p-3">
-          <h4>Colaboradores do Projeto</h4>
-          <hr />
-        </section>
+	    <section className="github mt-5">
+	    	<section className="page-header p-3">
+	    		<h4>Colaboradores do Projeto</h4>
+	    		<hr />
+	    	</section>
 
-        <section className="list container">
-          <section className="row">
-            <section className="col-6">
-              <a href="https://github.com/AnaSelma">
-                - José da Esquina
-                <FaGithub className="github-btn" />
-              </a>
+	    	<section className="list container">
+	    		<section className="row">
+	    			<section className="col-6">
+	    				<a href="https://github.com/adrianoavelino">
+			    			- Adriano Avelino
+			    			<img src={icon} alt="github icon" />
+		    			</a>
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
+		    			<a href="https://github.com/AnaSelma">
+			    			- Ana Selma
+			    			<img src={icon} alt="github icon" />
+		    			</a>
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
+		    			<a href="https://github.com/arturmnt">
+			    			-  Artur Meissner
+			    			<img src={icon} alt="github icon" />
+		    			</a>
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
+		    			<a href="https://github.com/CaioFML">
+			    			-  Caio Motta
+			    			<img src={icon} alt="github icon" />
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
+		    			</a><a href="https://github.com/kele90">
+			    			- César Martínez 
+			    			<img src={icon} alt="github icon" />
+		    			</a>		    			
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
+		    			<a href="https://github.com/chrystiamweb">
+			    			-  Chrystiam Andrade
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+							
+							<a href="https://github.com/DiegoRamires">
+			    			-  Diego Gonçalves
+			    			<img src={icon} alt="github icon" />
+		    			</a>
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
+		    			<a href="https://github.com/dbnkocao">
+			    			- Daniel B.do Nascimento
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
+			    			<img src={icon} alt="github icon" />
+		    			</a>
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
+		    			<a href="">
+			    			-  Fabricio Comeli Garcia B
+			    			<img src={icon} alt="github icon" />
+		    			</a>
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
-            </section>
+		    			<a href="https://github.com/f-vargas">
+			    			-  Frabricio Vargas
+			    			<img src={icon} alt="github icon" />
+		    			</a>
 
-            <section className="col-6">
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
+		    			<a href="https://github.com/GesielFreitas">
+			    			-  Gesiel Freitas
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+		    		</section>
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
+		    		<section className="col-6">
+		    			<a href="https://github.com/Guiarrd">
+			    			-  Guilherme Arruda
+			    			<img src={icon} alt="github icon" />
+		    			</a>
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
+		    			<a href="https://github.com/igornichele">
+			    			-  Igor Nichele
+			    			<img src={icon} alt="github icon" />
+		    			</a>
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
+		    			<a href="https://github.com/geonizeli">
+			    			-  João Geonizeli
+								<img src={icon} alt="github icon" />
+		    			</a>
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
+		    			<a href="https://github.com/lglinardello">
+			    			- Lilian
+			    			<img src={icon} alt="github icon" />
+		    			</a>
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
+		    			<a href="https://github.com/brandaoplaster">
+			    			-  Lucas Brandao
+			    			<img src={icon} alt="github icon" />
+		    			</a>
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
+		    			<a href="https://github.com/Lucasdfg07">
+			    			-  Lucas Siqueira Fernandes
+			    			<img src={icon} alt="github icon" />
+		    			</a>		    			
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
+		    			<a href="https://github.com/tiagompalte">
+			    			-  Tiago Mariani Palte
+			    			<img src={icon} alt="github icon" />
+		    			</a>
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
+		    			<a href="https://github.com/toticavalcantihttps://github.com/toticavalcanti">
+			    			-  Toti Cavalcanti
+			    			<img src={icon} alt="github icon" />
+		    			</a>
 
-              <a
-                href="https://github.com"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                -
-                <FaGithub className="github-btn" />
-              </a>
-            </section>
-          </section>
-        </section>
-      </section>
-    </section>
+		    			<a href="">
+			    			-  
+			    			<img src={icon} alt="github icon" />
+		    			</a>
+		    		</section>
+	    		</section>
+	    	</section>
+	    </section>	
+	</section>
   );
 }
